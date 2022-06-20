cb2d84acc06e3857066f6b07eb68014b186d1093
NickSchenck <yoyo4games@yahoo.com>
2022-06-20 18:31:50 -0500

    Validation implimented

--- app.js
+++ app.js
@@ -2,15 +2,125 @@ const inquirer = require('inquirer');
 // const fs = require("fs");
 // const generatePage = require("./src/page-template");
 
-inquirer
-    .prompt([
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: "input",
             name: "name",
-            message: "What is your name?"
+            message: "What is your name? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your name.");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your GitHub username: (Required)",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub username.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "about",
+            message: "Provide some information about yourself:"
         }
-    ])
-    .then(answers => console.log(answers));
+    ]);
+};
+
+const promptProject = portfolioData => {
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `)
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your project? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a name for your project.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Provide a description of the project (Required)",
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log("Please provide a description of your project.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "checkbox",
+            name: "languages",
+            message: "What did you build this project with? (Check all the apply)",
+            choices: ["JavaScript", "HTML", "CSS", "ES6", "jQuery", "Bootstrap", "Node"]
+        },
+        {
+            type: "input",
+            name: "link",
+            message: "Enter the GitHub link to your project. (Required)",
+            validate: linkInput => {
+                if (linkInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a valid GitHub project link.")
+                    return false;
+                }
+            }
+        },
+        {
+            type: "confirm",
+            name: "feature",
+            message: "Would you like to feature this project?",
+            default: false
+        },
+        {
+            type: "confirm",
+            name: "confirmAddProject",
+            message: "Would you like to enter another project?",
+            default: false
+        }
+    ]);
+};
+    promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        console.log(portfolioData);
+    })
+    // .then(projectAnswers => console.log(projectAnswers))
+    // .then(projectData => {
+    //     portfolioData.projects.push(projectData);
+    //     if (projectData.confirmAddProject) {
+    //         return promptProject(portfolioData);
+    //     } else {
+    //         return portfolioData;
+    //     }
+    // });
 // const pageHTML = generatePage(name, github);
 
 // fs.writeFile("./index.html", pageHTML, err => {
