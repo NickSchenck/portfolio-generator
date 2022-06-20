e668684e7eef6e1126d2b80c3a6967478f6d4320
NickSchenck <yoyo4games@yahoo.com>
2022-06-20 17:23:33 -0500

    first user interaction

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
+const inquirer = require('inquirer');
+// const fs = require("fs");
+// const generatePage = require("./src/page-template");
 
-const profileDataArgs = process.argv.slice(2)
+inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name?"
+        }
+    ])
+    .then(answers => console.log(answers));
+// const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
-// console.log(profileDataArgs);
+// fs.writeFile("./index.html", pageHTML, err => {
+//     if (err) throw err;
 
-// // Notice the lack of parentheses around the `profileDataArr` parameter?
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i+= 1){
-//     console.log(profileDataArr[i]);
-//     }
-//     console.log("================");
-
-//     //Is the same as this
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-
-// printProfileData(profileDataArgs);
-// console.log(name, github);
-// console.log(generatePage(name, github));
-fs.writeFile("./index.html", generatePage(name, github), err => {
-    if (err) throw new Error(err);
-
-    console.log("Portfolio complete, check out index.html to see the output.");
-});
+//     console.log("Portfolio complete, check out index.html to see the output.");
+// });
